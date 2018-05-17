ac06d9f3e82126138137d31c5da04366d9c2a82e
Aline Kimy Miyazaki <alinekm03@gmail.com>
2018-05-17 06:22:22 -0300

    Authentication

--- TaskManager/Global.asax.cs
+++ TaskManager/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using TaskManager.App_Start;
 
 namespace TaskManager
 {
---
+++ TaskManager/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+[assembly: OwinStartup(typeof(TaskManager.Startup))]
+
+namespace TaskManager
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "ApplicationCookie",
+                LoginPath = new PathString("/authentication/login")
+            });
+        }
+    }
+}
